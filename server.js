7e4aae32ee46a429a982c12e88885bd1e3f45109
fernando <fernandooj@ymail.com>
2016-08-06 01:05:39 -0500

    make it better

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-/*eslint no-console:0 */
-/*'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
+var express = require('express');
+var app = express();
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-  console.log('Opening your system browser...');
-  open('http://localhost:' + config.port + '/webpack-dev-server/');
-});*/
+app.use(express.static(__dirname));
 
-var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
- 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server listening');
+});
